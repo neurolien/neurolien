b846cee3ecefaf56f3ef46f3c0c76f703cecb1f2
billdevmaster71 <billdevmaster@gmail.com>
2023-09-19 05:47:05 +0900

    feat: update for pages

---
+++ src/routes/consult/success/+page.svelte
@@ -0,0 +1,9 @@
+
+<div class="text-center text-white w-[330px] py-32 bg-sky-400">
+  <p class="font-bold text-xl mb-6">
+    Your request has been sent!
+  </p>
+  <p class="font-bold text-xl">
+    Thank you for your trust :)
+  </p>
+</div>
---
+++ src/routes/share/success/+page.svelte
@@ -0,0 +1,21 @@
+<a href="/">
+  <img src="../logo.png" alt="" class="w-[100px] m-5">
+</a>
+<div class="flex flex-col justify-center items-center mt-12">
+  <div class="w-[330px]">
+    <p class="text-2xl bg-sky-400 w-[330px] py-3 font-bold text-center mb-12">Nos talents</p>
+    
+    <div>
+      <div class="bg-sky-400 p-5 mb-5">
+        <p class="text-white text-2xl font-bold mb-5">PSEUDO 1 A 4.2 KMS</p>
+        <p class="text-white text-2xl font-bold mb-5">+41 78 123 1252</p>
+        <p class="text-white text-3xl font-bold mb-5">Description</p>
+      </div>
+      <div class="bg-sky-400 p-5">
+        <p class="text-white text-2xl font-bold mb-5">PSEUDO 1 A 4.2 KMS</p>
+        <p class="text-white text-2xl font-bold mb-5">+41 78 123 1252</p>
+        <p class="text-white text-3xl font-bold mb-5">Description</p>
+      </div>
+    </div>
+  </div>
+</div>
