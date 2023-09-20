d9571a9ff04552646ec11a6d3a78bed0ff0ed8c5
billdevmaster71 <billdevmaster@gmail.com>
2023-09-20 16:31:00 +0900

    fix: logo png path in auth

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,6 +1,6 @@
 <div class="flex justify-center mt-32 mb-10">
   <a href="/" class="m-auto">
-    <img src="logo.png" alt="" class="w-[100px]">
+    <img src="../logo.png" alt="" class="w-[100px]">
   </a>
 </div>
 <slot />
