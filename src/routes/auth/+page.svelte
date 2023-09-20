5c131fd73c19cf7c7b3ceaffbdffbf2d4f91f9ce
billdevmaster71 <billdevmaster@gmail.com>
2023-09-20 10:24:50 +0900

    fix: redesign the mock pages

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,11 +1,14 @@
-<a href="/">
-  <img src="logo.png" alt="" class="w-[100px] m-5">
-</a>
-<div class="flex flex-col justify-center items-center mt-24">
-  <p class="text-2xl bg-sky-400 w-[330px] py-3 font-bold text-center mb-32">I am a talent</p>
-  <div class="flex mb-32">
-    <p class="text-2xl mr-5">Phone</p>
-    <input class="enabled:hover:border-gray-400 disabled:opacity-75 bg-sky-300 px-3 py-2" />
+<script>
+  import InputForm from "../../components/InputForm.svelte";
+  import Button from "../../components/Button.svelte";
+  import { goto } from "$app/navigation";
+</script>
+
+<div class="grid grid-cols-1">
+  <p class="text-4xl font-bold text-center text-sky-400 mb-24 ">Je suis un talent</p>
+  <div class="mb-24">
+    <InputForm label="Téléphone" />
   </div>
-  <a href="/auth-confirm" class="border border-4 border-black w-[330px] text-center text-xl font-bold py-3">Following</a>
+  
+  <Button on:click={() => goto("/auth/confirm")}>Suivante</Button>
 </div>
--- src/routes/share/main/+page.svelte
+++ src/routes/share/main/+page.svelte
@@ -1,26 +1,18 @@
-<a href="/">
-  <img src="../logo.png" alt="" class="w-[100px] m-5">
-</a>
-<div class="flex flex-col justify-center items-center mt-12">
-  <div class="w-[330px]">
-    <p class="text-2xl bg-sky-400 w-[330px] py-3 font-bold text-center mb-12">I'm looking for talent</p>
-    
-    <div class="flex mb-8">
-      <p class="text-xl w-[100px] text-center mr-5">First name</p>
-      <input class="enabled:hover:border-gray-400 disabled:opacity-75 bg-sky-300 px-3 py-2" />
-    </div>
-    <div class="flex mb-8">
-      <p class="text-xl w-[100px] text-center mr-5">Nom</p>
-      <input class="enabled:hover:border-gray-400 disabled:opacity-75 bg-sky-300 px-3 py-2" />
-    </div>
-    <div class="flex mb-8">
-      <p class="text-xl w-[100px] text-center mr-5">Ville</p>
-      <input class="enabled:hover:border-gray-400 disabled:opacity-75 bg-sky-300 px-3 py-2" />
-    </div>
-    <div class="mb-8 w-full">
-      <p class="text-xl mr-5">My research:</p>
-      <textarea class="bg-sky-300 w-full p-5" cols="30" rows="7"></textarea>
-    </div>
-    <a href="/share/success" class="border border-4 border-black w-full block text-center text-xl font-bold py-3">Following</a>
+
+<script>
+  import InputForm from "../../../components/InputForm.svelte";
+  import TextArea from "../../../components/TextArea.svelte";
+  import Button from "../../../components/Button.svelte";
+  import { goto } from "$app/navigation";
+</script>
+
+<div class="grid grid-cols-1">
+  <p class="text-4xl font-bold text-center text-sky-400 mb-24">je recherche des talents</p>
+  <InputForm label="Prénom"/>
+  <InputForm label="Nom"/>
+  <InputForm label="Ville"/>
+  <TextArea label="Ma recherche:" />
+  <div class="mt-10">
+    <Button on:click={() => goto("/share/success")}>Suivante</Button>
   </div>
 </div>
